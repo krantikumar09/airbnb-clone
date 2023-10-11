f01499337ce3c3cc89371b82993cbb425cb727a9
Krantikumar Mathapati <krantikumar3446@gmail.com>
2023-10-11 15:01:20 +0530

    Created header with basic functionality

---
+++ src/js/main.js
@@ -0,0 +1,6 @@
+// handle user menu
+const userMenu = document.getElementById("userMenu");
+
+function handleUserMenu() {
+  userMenu.classList.toggle("active");
+}
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+
+module.exports = {
+  content: [],
+  theme: {
+    screens: {
+      sm: "640px",
+    },
+    extend: {},
+  },
+  plugins: [],
+};
