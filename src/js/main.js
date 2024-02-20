2b45cf1e8abac7765e65af7c52bd984d1c6b7bc6
Krantikumar Mathapati <krantikumar3446@gmail.com>
2024-02-20 21:53:06 +0530

    created filter section

--- src/js/main.js
+++ src/js/main.js
@@ -11,3 +11,30 @@ $(document).ready(function () {
     $("#searchNav").toggle();
   });
 });
+
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 6,
+  spaceBetween: 10,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+    300: {
+      slidesPerView: 2,
+      spaceBetween: 10,
+    },
+    420: {
+      slidesPerView: 3,
+      spaceBetween: 10,
+    },
+    640: {
+      slidesPerView: 4,
+      spaceBetween: 20,
+    },
+    1200: {
+      slidesPerView: 6,
+      spaceBetween: 40,
+    },
+  },
+});
